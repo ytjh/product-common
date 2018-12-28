a5da200549ab830395b361d5ccf0b1e5cce079a2
yst <463861615@qq.com>
2018-12-28 18:09:00 +0800

    gengxin

--- product-common/src/main/java/cn/vo/backstage/Utils/PageUtils.java
+++ product-common/src/main/java/cn/vo/backstage/Utils/PageUtils.java
@@ -14,13 +14,8 @@ public class PageUtils {
 		
 	}
 	public static Integer getPageSize(Integer page,Integer size){
-		int pageindex=page-1;
-		if(pageindex>0){
-			pageindex=pageindex*10;
-		}
-		int pageSize=page*10;
+		int pageSize=page*size;
 		return pageSize;
-		
 	}
 	
 
